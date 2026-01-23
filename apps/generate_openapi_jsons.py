d442f6463c1120e5f97c93b38e39b1ce99e6f5ce
Chris OBryan <13701027+cobryan05@users.noreply.github.com>
2026-01-23 16:55:29 -0600

    Allow Task Server to depend on other openapis

--- apps/generate_openapi_jsons.py
+++ apps/generate_openapi_jsons.py
@@ -5,43 +5,39 @@ import os
 import sys
 os.environ["GENERATING_OPENAPI_CLIENTS"] = "1"
 
-generate_backend = False
-generate_frontend = False
-
 if len(sys.argv) < 2:
-    print("Usage: python -m apps.generate_openapi_jsons [frontend|backend]")
+    print("Usage: python -m apps.generate_openapi_jsons <targets...> [output_dir]")
     sys.exit(1)
 
-arg = sys.argv[1].lower()
-if arg == "backend":
-    generate_backend = True
-elif arg == "frontend":
-    generate_frontend = True
-else:
-    print(f"Unknown option: {arg}")
-    print("Usage: python -m apps.generate_openapi_jsons [frontend|backend]")
-    sys.exit(1)
+args = sys.argv[1:]
+output_dir = "."
+if args[-1].startswith(("/", ".")):
+    output_dir = args.pop()
 
-output_dir = sys.argv[2] if len(sys.argv) >= 3 else "."
+targets = [a.lower() for a in args]
 
-if generate_backend:
-    from .relay_buffer_server.main import app as relay_server_app
-    with open(os.path.join(output_dir, "relay_buffer_server_openapi.json"), "w", encoding="utf-8") as f:
-        json.dump(relay_server_app.openapi(), f, indent=2)
-
-    from .inference_server.main import app as inference_server_app
-    with open(os.path.join(output_dir, "inference_server_openapi.json"), "w", encoding="utf-8") as f:
-        json.dump(inference_server_app.openapi(), f, indent=2)
-
-    from .tasks_server.main import app as tasks_server_app
-    with open(os.path.join(output_dir, "tasks_server_openapi.json"), "w", encoding="utf-8") as f:
-        json.dump(tasks_server_app.openapi(), f, indent=2)
-
+# simple ifs for each known target
+if "db" in targets:
     from .db_server.main import app as db_server_app
     with open(os.path.join(output_dir, "db_server_openapi.json"), "w", encoding="utf-8") as f:
         json.dump(db_server_app.openapi(), f, indent=2)
 
-if generate_frontend:
+if "relay" in targets:
+    from .relay_buffer_server.main import app as relay_server_app
+    with open(os.path.join(output_dir, "relay_buffer_server_openapi.json"), "w", encoding="utf-8") as f:
+        json.dump(relay_server_app.openapi(), f, indent=2)
+
+if "inference" in targets:
+    from .inference_server.main import app as inference_server_app
+    with open(os.path.join(output_dir, "inference_server_openapi.json"), "w", encoding="utf-8") as f:
+        json.dump(inference_server_app.openapi(), f, indent=2)
+
+if "tasks" in targets:
+    from .tasks_server.main import app as tasks_server_app
+    with open(os.path.join(output_dir, "tasks_server_openapi.json"), "w", encoding="utf-8") as f:
+        json.dump(tasks_server_app.openapi(), f, indent=2)
+
+if "image_tagging" in targets or "frontend" in targets:
     from .image_tagging_server.main import app as image_tagging_server_app
     with open(os.path.join(output_dir, "image_tagging_server_openapi.json"), "w", encoding="utf-8") as f:
-            json.dump(image_tagging_server_app.openapi(), f, indent=2)
+        json.dump(image_tagging_server_app.openapi(), f, indent=2)
--- generate_openapi_modules.sh
+++ generate_openapi_modules.sh
@@ -8,20 +8,23 @@ mkdir -p "$JSON_OUT_DIR"
 # Create temporary directory
 TMP_DIR=$(mktemp -d)
 
-# Generate the JSONs
-python -m apps.generate_openapi_jsons backend "$JSON_OUT_DIR"
+# Generate documentation for the backends. Must install in order of dependencies
+python -m apps.generate_openapi_jsons db relay inference "$JSON_OUT_DIR"
 
-# Generate the python API modules from the JSONs into the temp dir
+# Generate and install the python API modules from the JSONs
 openapi-generator-cli generate -i "$JSON_OUT_DIR/relay_buffer_server_openapi.json" -g python -o "$TMP_DIR/relay_buffer_client" --package-name relay_buffer_client
 openapi-generator-cli generate -i "$JSON_OUT_DIR/inference_server_openapi.json" -g python -o "$TMP_DIR/inference_client" --package-name inference_client
-openapi-generator-cli generate -i "$JSON_OUT_DIR/tasks_server_openapi.json" -g python -o "$TMP_DIR/tasks_client" --package-name tasks_client
 openapi-generator-cli generate -i "$JSON_OUT_DIR/db_server_openapi.json" -g python -o "$TMP_DIR/db_client" --package-name db_client
 
-# Install the generated clients
-pip install --no-cache-dir "$TMP_DIR"/relay_buffer_client "$TMP_DIR"/inference_client "$TMP_DIR"/tasks_client "$TMP_DIR"/db_client
+pip install --no-cache-dir "$TMP_DIR"/relay_buffer_client "$TMP_DIR"/inference_client "$TMP_DIR"/db_client
+
+# Now generate the tasks server
+python -m apps.generate_openapi_jsons tasks "$JSON_OUT_DIR"
+openapi-generator-cli generate -i "$JSON_OUT_DIR/tasks_server_openapi.json" -g python -o "$TMP_DIR/tasks_client" --package-name tasks_client
+pip install --no-cache-dir "$TMP_DIR"/tasks_client
 
 # Generate the Javascript API for the web front end as well
-python -m apps.generate_openapi_jsons frontend "$JSON_OUT_DIR"
+python -m apps.generate_openapi_jsons image_tagging "$JSON_OUT_DIR"
 python -m apps.generate_jsdoc_from_openapi_json "$JSON_OUT_DIR/image_tagging_server_openapi.json" "static/js/api-types.js"
 
 rm -rf "$TMP_DIR"
